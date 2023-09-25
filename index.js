8977d123e23e7ce2a0529643ffa8e33da221a643
Mateusz Wójcikowski <yankes90@gmail.com>
2023-09-25 19:37:45 +0200

    route test

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
-/*
- * Copyright (c) 2023 genshin.dev & MJW
- * Licensed under the Open Software License version 3.0
- */
-import Koa from 'koa';
-import { koaBody } from 'koa-body';
-import helmet from 'koa-helmet';
-import cors from '@koa/cors';
-import chalk from 'chalk';
+import Koa from "koa";
+import { koaBody } from "koa-body";
+import helmet from "koa-helmet";
+import cors from "@koa/cors";
+import chalk from "chalk";
+import { default as router } from "./routes/router.js";
 
-// import router from './routes';
+const app = new Koa();
+const port = process.env.PORT || 3000;
 
+app.use(koaBody());
+app.use(helmet());
+app.use(cors());
 
-(async () => {
-  const app = new Koa();
-  const port = process.env.PORT || 3000;
+app.use(router.routes());
 
-  app.use((ctx) => {
-    ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`;
-  });
-  
-  app.use(koaBody());
-  app.use(helmet());
-  app.use(cors());
-
-
-  // app.use(router.routes());
-
-  app.listen(port, () => {
-    console.log(
-      chalk.blue('[API]'),
-      'Running on',
-      chalk.yellow(`0.0.0.0:${port}`),
-    );
-  });
-})();
+app.listen(port, () => {
+  console.log(
+    chalk.blue("[API]"),
+    "Running on",
+    chalk.yellow(`0.0.0.0:${port}`)
+  );
+});
